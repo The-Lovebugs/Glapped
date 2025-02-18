d8afba22ccfdbf3c8df5d76ed7c0c5003325029e
dylan hough <dylanpjhough@gmail.com>
2025-02-18 00:44:54 +0000

    register added

--- Glapped/Glapped/settings.py
+++ Glapped/Glapped/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_bootstrap4',
+    'crispy_forms',
     'Glapped_main',
+    'register',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- Glapped/Glapped/urls.py
+++ Glapped/Glapped/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from register.views import register
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Glapped_main.urls')),
+    path('register/', register, name='register'),
 ]
