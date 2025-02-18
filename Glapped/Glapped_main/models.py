8d28820c03ff61695b6f2c96ccd4110c9625eb46
Alex <72073347+Cosmospacedog@users.noreply.github.com>
2025-02-18 11:02:44 +0000

    add users table

--- Glapped/Glapped_main/models.py
+++ Glapped/Glapped_main/models.py
@@ -5,8 +5,14 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
     price = models.FloatField()
     description = models.TextField()
     #image = models.ImageField(upload_to='product_images')
-    category = models.CharField(max_length=100)
+    category = models.CharField(max_length=255)
+
+class Member(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    
