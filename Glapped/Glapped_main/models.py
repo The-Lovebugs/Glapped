15283ff26e2b0d9e6c23ac7122f997e588ceb2bd
dylan hough <dylanpjhough@gmail.com>
2025-02-18 13:14:43 +0000

    model not needed

--- Glapped/Glapped_main/models.py
+++ Glapped/Glapped_main/models.py
@@ -9,9 +9,3 @@ class Product(models.Model):
     description = models.TextField()
     #image = models.ImageField(upload_to='product_images')
     category = models.CharField(max_length=255)
-
-class Member(models.Model):
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
-    username = models.CharField(max_length=255)
-    
