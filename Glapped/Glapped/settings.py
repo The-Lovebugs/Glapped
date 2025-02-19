a0bbc11d773a6f36ef380413d112dda0e6f99bb0
Alex <alex.burnett7@gmail.com>
2025-02-19 23:08:59 +0000

    logout now goes to homepage instead of straight back in and beginning to style the page creation form

--- Glapped/Glapped/settings.py
+++ Glapped/Glapped/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-LOGOUT_REDIRECT_URL = '/login' #anywhere you like
+LOGOUT_REDIRECT_URL = '/' #anywhere you like
 LOGIN_REDIRECT_URL = '/' #anywhere you like
 
 # Static files (CSS, JavaScript, images)
