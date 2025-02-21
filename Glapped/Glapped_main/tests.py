cb7e5fdcb55b1572e9a173b00a39b223680638eb
janvanwingerden <januarywastaken@gmail.com>
2025-02-21 23:42:05 +0000

    Made edits. Still not successful

--- Glapped/Glapped_main/tests.py
+++ Glapped/Glapped_main/tests.py
@@ -1,17 +1,28 @@
 from django.test import TestCase
 from .models import Product
+from .forms import CreateNewListing
 
 
 class ProductTestCase(TestCase):   
     def setUp(self):
         self.productdetails = {
-            'name': 'Blue Jeans',
+            'title': 'Blue Jeans',
             'price': 50,
             'description': 'Hole over left knee',
-            'image': 'resources/default.jpg',
-            'category': 'Clothes'}
+            'image': 'resources/default.jpg'}
+    def test_form(self):
+        form = CreateNewListing(data=self.productdetails)
+        instances = Product.objects.all()
+        print("\nPrinting all MyModel instances:")
+        for instance in instances:
+            print(instance.name)
+        self.assertTrue(form.is_valid())
     def test_createListing(self):
         # send product data
         self.client.post('/createlisting/', self.productdetails, follow=True)
         # Check if the user is created in the database
-        self.assertTrue(Product.objects.filter(name=self.productdetails['name']).exists())
+        instances = Product.objects.all()
+        print("\nPrinting all MyModel instances:")
+        for instance in instances:
+            print(instance)
+        self.assertTrue(Product.objects.filter(name=self.productdetails['title']).exists())
