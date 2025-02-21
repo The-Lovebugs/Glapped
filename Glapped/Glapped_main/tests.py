86fb795d49eb2502db24f803bef3cb2e55f3549a
janvanwingerden <januarywastaken@gmail.com>
2025-02-21 17:22:05 +0000

    Wrote some test cases. Don't run when test is called :(

--- Glapped/Glapped_main/tests.py
+++ Glapped/Glapped_main/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+
+class ProductTestCase(TestCase):   
+    def setUp(self):
+        self.productdetails = {
+            'name': 'Blue Jeans',
+            'price': 50,
+            'description': 'Hole over left knee',
+            'image': 'resources/default.jpg',
+            'category': 'Clothes'}
+    def test_createListing(self):
+        # send product data
+        response = self.client.post('/createlisting/', self.productdetails, follow=True)
+        # Check if the user is created in the database
+        self.assertTrue(Product.objects.filter(username=self.productdetails['name']).exists())
--- Glapped/register/tests.py
+++ Glapped/register/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import UserProfile
 
 # Create your tests here.
+class LogInTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'P455word!'}
+        self.client.post('/register/', self.credentials, follow=True)
+    def test_login(self):
+        # send login data
+        response = self.client.post('/login/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
+    def test_account_created(self):
+        # Check if the user is created in the database
+        self.assertTrue(User.objects.filter(username=self.credentials['username']).exists()) 
+    def test_userprofile_created(self):
+        # reference created user account
+        user = User.objects.get(username=self.credentials['username'])
+        #test is userprofile has been created
+        try:
+            #testing if userprofile exists
+            profile = UserProfile.objects.get(user=user)
+            self.assertIsNotNone(profile)
+        except UserProfile.DoesNotExist:
+            self.fail("UserProfile was not created for the user.")
