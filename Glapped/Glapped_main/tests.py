9dbbdd14608a6f3f94808e2c4f278f6c400ab660
janvanwingerden <januarywastaken@gmail.com>
2025-02-24 11:43:07 +0000

    Tests for glapped_main work akfgui;afdl

--- Glapped/Glapped_main/tests.py
+++ Glapped/Glapped_main/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from .models import Product
 from .forms import CreateNewListing
+import os
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 
 class ProductTestCase(TestCase):   
@@ -8,18 +10,21 @@ class ProductTestCase(TestCase):
         self.productdetails = {
             'title': 'Blue Jeans',
             'price': 50,
-            'description': 'Hole over left knee',
-            'image': 'resources/default.jpg'}
+            'description': 'Hole over left knee'}
+        cur_path = os.path.dirname(__file__)
+        new_path = os.path.join(cur_path, '..', 'media', 'resources', 'default.jpg')
+        with open(new_path, 'rb') as file:
+            self.image = SimpleUploadedFile(
+                name='default.jpg',
+                content=file.read(),
+                content_type='image/jpeg'
+            )
     def test_form(self):
-        form = CreateNewListing(data=self.productdetails)
-        instances = Product.objects.all()
-        print("\nPrinting all MyModel instances:")
-        for instance in instances:
-            print(instance.name)
+        form = CreateNewListing(data=self.productdetails, files={'image': self.image})
         self.assertTrue(form.is_valid())
     def test_createListing(self):
         # send product data
-        self.client.post('/createlisting/', self.productdetails, follow=True)
+        self.client.post('/createlisting/', data={**self.productdetails, 'image':self.image}, follow=True)
         # Check if the user is created in the database
         instances = Product.objects.all()
         print("\nPrinting all MyModel instances:")
